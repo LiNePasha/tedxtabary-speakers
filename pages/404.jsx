88d7ecd4b8225f80df49bab23ccd14a2b46bb8e2
ahmed992 <ahmedalkpeer.v2@gmail.com>
2021-11-08 17:30:26 +0200

    footer to error page

--- pages/404.jsx
+++ pages/404.jsx
@@ -21,6 +21,20 @@ function Error() {
         <Link  href="/"><a class="default-button">الرجوع الي الصفحة المتحدثين</a></Link>
       </div>
     </section>
+
+    <footer class="footer-seven">
+            <div class="overlay">
+                <div class="container">
+                    <h2>الصفحة غير موجودة</h2>
+                    <ul class="event-social">
+                        <li><a target="_blank" href="https://www.facebook.com/TEDxYouthTabaryElHegazHS"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                        <li><a target="_blank" href="https://www.instagram.com/tedxtabaryelhegaz/"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                    </ul>
+                    <a target="_blank" href="https://tedxtabaryelhegaz.com" className="btn btn-danger">TedxTabaryElhegaz.com</a>
+                    <p>&copy; جميع الحقوق محفوظة لدي <a href="https://tedxtabaryelhegaz.com">تيدكس طبري الحجاز</a>. صممت بواسطة <a target="_blank" href="https://ahmedwaleed.xyz/">أحمد وليد</a></p>
+                </div>
+            </div>
+        </footer>
     </>
   )
 }
